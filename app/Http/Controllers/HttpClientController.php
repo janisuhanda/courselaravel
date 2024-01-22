0341befabaa357a32e2c300445f771243ba8dc4a
janisuhanda <janisuhanda@outlook.co.id>
2024-01-22 19:00:18 +0700

    mmm

--- app/Http/Controllers/HttpClientController.php
+++ app/Http/Controllers/HttpClientController.php
@@ -12,8 +12,15 @@ class HttpClientController extends Controller
         // $response = Http::get('https://jsonplaceholder.typicode.com/posts');
         // dd($response->body());
 
-        $client = new \GuzzleHttp\Client();
-        $res = $client->request('GET', 'https://jsonplaceholder.typicode.com/posts');
+        $client = new \GuzzleHttp\Client(
+            [
+                'defaults' => [
+                    'exceptions' => false
+                ]
+            ]
+        );
+        // $res = $client->request('GET', 'https://jsonplaceholder.typicode.com/posts');
+        $res = $client->request('GET', 'http://localhost:8000/api/readpost');
         // echo $res->getStatusCode();
         // echo "<pre>";
         // print_r(json_decode($res->getBody()->getContents()));
